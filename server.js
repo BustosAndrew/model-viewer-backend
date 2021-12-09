64cc2952451ad42de251a82a66f92f3394e16917
BustosMan <infernious@live.com>
2021-12-09 01:25:56 -0800

    Hello world msg on /

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
 app.get("/credentials", (req, res) => {
     res.send({
         apiKey: process.env.API_KEY,
